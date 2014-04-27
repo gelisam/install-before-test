78aa091dc33d361aea76e1b0e1abe52ee3731287
Samuel Gélineau <gelisam+github@gmail.com>
2014-04-27 08:58:08 -0400

    preserve existing arguments

--- Setup.hs
+++ Setup.hs
@@ -2,9 +2,19 @@ import Control.Monad
 import Distribution.Simple
 import System.Environment
 
+substitute :: Eq a => [(a, [a])] -> [a] -> [a]
+substitute substitutions = (>>= go)
+  where
+    go x = case lookup x substitutions of
+             Just xs -> xs
+             Nothing -> return x
+
 main = do
     args <- getArgs
     when ("test" `elem` args) $ do
       -- unlike most packages, Hawk needs to be installed before it can be tested.
-      defaultMainArgs ["install"]
+      defaultMainArgs (substitute [("test", ["install"])] args)
+    when ("install" `elem` args && "--enable-tests" `elem` args) $ do
+      -- first, install without the tests.
+      defaultMainArgs (substitute [("--enable-tests", [])] args)
     defaultMainArgs args
